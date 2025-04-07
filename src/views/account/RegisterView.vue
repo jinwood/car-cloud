184b009f56b748aa6f221426f6f9216d210df2ca
jinwood <jinw@protonmail.com>
2025-04-07 22:02:02 +0100

    Starting doing reg but too tired

--- src/views/account/RegisterView.vue
+++ src/views/account/RegisterView.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import BaseButton from '@/components/ui/BaseButton.vue'
+import BaseCard from '@/components/ui/BaseCard.vue'
+import TextInput from '@/components/ui/TextInput.vue'
+</script>
 <template>
-  <div>register</div>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <BaseCard><h2 class="text-2xl font-semibold">Register</h2></BaseCard>
+    <BaseCard>
+      <TextInput :id="''" />
+    </BaseCard>
+  </div>
 </template>
