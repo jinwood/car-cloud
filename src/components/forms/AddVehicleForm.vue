804831f07245062dabb9152592889380155bf394
jinwood <jinw@protonmail.com>
2025-05-13 21:50:56 +0100

    Form

--- src/components/forms/AddVehicleForm.vue
+++ src/components/forms/AddVehicleForm.vue
@@ -3,69 +3,72 @@
     @submit.prevent="handleSubmit"
     class="dark:bg-gray-800 dark:text-gray-100 bg-white text-gray-800 rounded-lg shadow-md p-6 grid grid-cols-1 gap-6"
   >
-    <h3 class="text-xl font-semibold">Add New Vehicle</h3>
-    <div>
-      <label for="make" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Make:</label
-      >
-      <TextInput type="text" v-model="formData.make" id="make" required />
-    </div>
-    <div>
-      <label for="model" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Model:</label
-      >
-      <TextInput type="text" v-model="formData.model" id="model" required />
-    </div>
-    <div>
-      <label for="reg" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Registration:</label
-      >
-      <TextInput type="text" v-model="formData.registrationNumber" id="reg" />
-    </div>
-    <div>
-      <label for="fuelType" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Fuel Type:</label
-      >
-      <select
-        v-model="formData.fuelType"
-        id="fuelType"
-        required
-        class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      >
-        <option v-for="type in fuelTypes" :key="type" :value="type">{{ type }}</option>
-      </select>
-    </div>
-    <div>
-      <label for="engineSize" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Engine Size:</label
-      >
-      <TextInput type="text" v-model="formData.engineSize" id="engineSize" />
-    </div>
-    <div>
-      <label for="bodyTypes" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-        >Body Type:</label
-      >
-      <select
-        v-model="formData.bodyType"
-        id="bodyTypes"
-        required
-        class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      >
-        <option v-for="bodyType in bodyTypes" :key="bodyType" :value="bodyType">
-          {{ bodyType }}
-        </option>
-      </select>
-    </div>
+    <BaseCard :split-columns="true">
+      <div>
+        <label for="make" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Make:</label
+        >
+        <TextInput type="text" v-model="formData.make" id="make" required />
+      </div>
+      <div>
+        <label for="model" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Model:</label
+        >
+        <TextInput type="text" v-model="formData.model" id="model" required />
+      </div>
+      <div>
+        <label for="reg" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Registration:</label
+        >
+        <TextInput type="text" v-model="formData.registrationNumber" id="reg" />
+      </div>
+      <div>
+        <label for="fuelType" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Fuel Type:</label
+        >
+        <select
+          v-model="formData.fuelType"
+          id="fuelType"
+          required
+          class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option v-for="type in fuelTypes" :key="type" :value="type">{{ type }}</option>
+        </select>
+      </div>
+      <div>
+        <label for="engineSize" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Engine Size:</label
+        >
+        <TextInput type="text" v-model="formData.engineSize" id="engineSize" />
+      </div>
+      <div>
+        <label for="bodyTypes" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+          >Body Type:</label
+        >
+        <select
+          v-model="formData.bodyType"
+          id="bodyTypes"
+          required
+          class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option v-for="bodyType in bodyTypes" :key="bodyType" :value="bodyType">
+            {{ bodyType }}
+          </option>
+        </select>
+      </div>
 
-    <BaseButton type="submit" :disabled="isSubmitting">
-      {{ isSubmitting ? 'Adding...' : 'Add Vehicle' }}
-    </BaseButton>
-    <p v-if="submitError" class="text-red-500">{{ submitError }}</p>
+      <BaseButton type="submit" :disabled="isSubmitting">
+        {{ isSubmitting ? 'Adding...' : 'Add Vehicle' }}
+      </BaseButton>
+      <p v-if="submitError" class="text-red-500">{{ submitError }}</p>
+    </BaseCard>
   </form>
 </template>
 
 <script setup lang="ts">
 import TextInput from '../ui/TextInput.vue'
+import BaseCard from '../ui/BaseCard.vue'
+import BaseButton from '../ui/BaseButton.vue'
 import { ref, reactive } from 'vue'
 import { useVehicles } from '@/stores/useVehicleStore'
 import { BodyType, FuelType } from '@/types'
--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,7 +1,19 @@
 <template>
   <div
-    class="dark:bg-gray-800 dark:text-gray-100 bg-white text-gray-800 rounded-lg shadow-md p-6 grid grid-cols-1 gap-6"
+    class="dark:bg-gray-800 dark:text-gray-100 bg-white text-gray-800 rounded-lg p-6 grid grid-cols-1 gap-6"
+    :class="{ 'md:grid-cols-2': splitColumns }"
   >
     <slot></slot>
   </div>
 </template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  splitColumns: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
