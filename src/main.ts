c8c2bb9ec098c831b9811845cd11a8d44f87e077
jinwood <jinw@protonmail.com>
2025-07-20 22:51:44 +0100

    Add firebase auth

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './firebase'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+
+const auth = useFirebaseAuth()
+console.log(auth)
 </script>
 
 <template>
